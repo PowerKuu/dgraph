8709ddd79174f4fbf3be1fd8fc6d5af81e1f4199
PowerKuu <hkleven6@outlook.com>
2022-11-25 22:34:37 +0100

    update

--- utils/logger.js
+++ utils/logger.js
@@ -21,30 +21,3 @@ function success(state, message) {
 exports.error = error
 exports.info = info
 exports.success = success
-
-
-
-
-
-
-
-
-
-fetch("http://localhost:8080/alter", {
-  "headers": {
-    "accept": "*/*",
-    "accept-language": "nb-NO,nb;q=0.9,no;q=0.8,nn;q=0.7,en-US;q=0.6,en;q=0.5",
-    "content-type": "text/plain;charset=UTF-8",
-    "sec-ch-ua": "\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"",
-    "sec-ch-ua-mobile": "?1",
-    "sec-ch-ua-platform": "\"Android\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site",
-    "x-auth-token": "undefined",
-    "Referer": "http://localhost:8000/",
-    "Referrer-Policy": "strict-origin-when-cross-origin"
-  },
-  "body": "{\"drop_op\":\"DATA\"}",
-  "method": "POST"
-});
